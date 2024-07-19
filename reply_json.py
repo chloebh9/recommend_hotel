44ab7ef9798f0d3b7da044dff20da75d31725d10
Hyeonsang Cho <67507183+hyeonsang010716@users.noreply.github.com>
2024-07-19 23:09:52 +0900

    [feature delete]

--- reply_json.py
+++ reply_json.py
@@ -1,20 +1,20 @@
-from question_answer_chain import init_retriver, init_chain, ask_something
-from dotenv import load_dotenv
+# from question_answer_chain import init_retriver, init_chain, ask_something
+# from dotenv import load_dotenv
 
 
-def get_json(query):
-    load_dotenv()  # .env 파일에서 환경 변수 로드
+# def get_json(query):
+#     load_dotenv()  # .env 파일에서 환경 변수 로드
 
-    filepath = "/root/LLM_Bootcamp/exercise_3/data/cleaned_top_200_rows (1).txt"
+#     filepath = "/root/LLM_Bootcamp/exercise_3/data/cleaned_top_200_rows (1).txt"
 
-    # 리트리버 초기화
-    retriever = init_retriver(filepath)
-    # 체인 초기화
-    rag_chain  = init_chain(retriever)
+#     # 리트리버 초기화
+#     retriever = init_retriver(filepath)
+#     # 체인 초기화
+#     rag_chain  = init_chain(retriever)
 
-    # 체인을 통해 사용자 질문을 처리하고 출력
-    chain_output = rag_chain.invoke(
-        {"input": query}
-    )
+#     # 체인을 통해 사용자 질문을 처리하고 출력
+#     chain_output = rag_chain.invoke(
+#         {"input": query}
+#     )
 
-    return chain_output
+#     return chain_output
