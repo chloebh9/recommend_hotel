a3c8d90f3039126d4d9a93651bf7d1af4827a4cf
iinmv2486 <127082547+iinmv2486@users.noreply.github.com>
2024-07-19 14:58:16 +0900

    get_json함수를 실행

--- reply_json.py
+++ reply_json.py
@@ -2,21 +2,19 @@ from question_answer_chain import init_retriver, init_chain, ask_something
 from dotenv import load_dotenv
 
 
-load_dotenv()  # .env 파일에서 환경 변수 로드
+def get_json(query):
+    load_dotenv()  # .env 파일에서 환경 변수 로드
 
-filepath = "/root/LLM_Bootcamp/exercise_3/data/cleaned_top_200_rows (1).txt"
+    filepath = "/root/LLM_Bootcamp/exercise_3/data/cleaned_top_200_rows (1).txt"
 
-# 리트리버 초기화
-retriever = init_retriver(filepath)
-# 체인 초기화
-rag_chain  = init_chain(retriever)
+    # 리트리버 초기화
+    retriever = init_retriver(filepath)
+    # 체인 초기화
+    rag_chain  = init_chain(retriever)
 
-# 사용자 입력 목록
-human_inputs = [
-    "서울에서 지낼만 한 호텔 추천해줘.",
-    "주변에 놀 곳이 많으면 좋을 겠어.",
-]
+    # 체인을 통해 사용자 질문을 처리하고 출력
+    chain_output = rag_chain.invoke(
+        {"input": query}
+    )
 
-# 각 사용자 입력에 대해 질문
-for input in human_inputs:
-    ask_something(rag_chain, input)
+    return chain_output
