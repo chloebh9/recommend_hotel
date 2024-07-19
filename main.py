261f2a76c3e7f8b8ef75235bb21c5f3a1fd1899b
co2ma <67058333+co2ma@users.noreply.github.com>
2024-07-19 15:29:32 +0900

    main commit

---
+++ main.py
@@ -0,0 +1,159 @@
+import streamlit as st
+import sqlite3
+import json
+import requests
+from PIL import Image
+from io import BytesIO
+
+def show_details(user):
+    with col2:
+        st.markdown('<div class="result-item">', unsafe_allow_html=True)
+        st.markdown(f'<h3 class="title">{user["firstName"]} {user["lastName"]}</h3>', unsafe_allow_html=True)
+        st.markdown(f'<p class="label">전화번호: {user["phoneNumber"]}</p>', unsafe_allow_html=True)
+        st.markdown(f'<p class="label">이메일: {user.get("emailAddress", "")}</p>', unsafe_allow_html=True)
+        st.markdown(f'<a href="{user["homepage"]}" class="top-right-btn" target="_blank">사이트 이동</a>', unsafe_allow_html=True)
+        st.markdown('</div>', unsafe_allow_html=True)
+        
+# SQLite 데이터베이스 연결
+conn = sqlite3.connect('example.db')
+cursor = conn.cursor()
+
+# Streamlit 앱 구현
+st.set_page_config(layout="wide")
+st.title('숙박 추천 시스템')
+
+# CSS 스타일 정의
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url('https://i.ibb.co/cYVPKqq/backgroundimage.jpg');
+        background-size: cover;
+        background-attachment: fixed;
+    }
+    .stApp::before {
+        position: absolute;
+        content: "";
+        top:0px;
+        left:0px;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(255,255,255,0.7);
+    }
+    body {
+    }
+    .centered {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+    .stTextInput input {
+        text-align: left;
+        font-size: 1.5em;
+    }
+    .search-container {
+        display: flex;
+        align-items: center;
+    }
+    .search-container input {
+        margin-right: 10px;
+    }
+    .result-item {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 24px;
+        gap: 12px;
+        margin: 10px;
+        width: 480px;
+        height: 208px;
+
+        background: #1E1E1F;
+        box-shadow: 0px 0px 3px rgba(18, 18, 20, 0.05), 0px 8px 16px rgba(18, 18, 20, 0.1);
+        border-radius: 24px;
+        position: relative;
+        overflow: hidden;
+    }
+    .title{
+        width: 600px;
+        height: 44px;
+
+        font-family: 'Inter';
+        font-style: normal;
+        font-weight: 800;
+        font-size: 38px;
+        line-height: 44px;
+        /* identical to box height, or 116% */
+        letter-spacing: -0.2px;
+
+        color: #EDEDED;
+    }
+    .label{
+        font-family: 'Inter';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 16px;
+        line-height: 4px;
+        color: #EDEDED;
+    }
+
+    .top-right-btn,
+    .bottom-right-btn {
+        position: absolute;
+        background-color: #E0E0E0; /* 버튼 배경색 */
+        color: #fff; /* 버튼 텍스트 색상 */
+        border: none; /* 버튼 테두리 제거 */
+        padding: 10px 20px; /* 버튼 내부 여백 */
+        cursor: pointer; /* 마우스 커서를 포인터로 변경 */
+        border-radius: 10px;
+        width: 100px;
+        font-size: 12px;
+        color: #000000;
+    }
+
+    .top-right-btn {
+        top: 10px;
+        right: 10px;
+    }
+
+    .bottom-right-btn {
+        bottom: 10px;
+        right: 10px;
+    }
+    
+    """,
+    unsafe_allow_html=True
+)
+
+# 중앙 정렬을 위한 컨테이너
+st.markdown('<div class="search-container">', unsafe_allow_html=True)
+search_term = st.text_input('', placeholder='다음과 같이 입력해 보세요! "바다가 보이는 호텔을 추천해줘."', key='search_input')
+st.markdown('</div>', unsafe_allow_html=True)
+col1, col2 = st.columns(2)
+
+with col1:
+    if search_term:
+    # 예시로 숙박 정보를 가져오는 API 호출
+        with open("/root/LLM_Bootcamp/exercise_3/test.json", "r") as json_file:
+            data = json.load(json_file)
+            # 결과 표시
+            st.markdown('<div class="search-results">', unsafe_allow_html=True)
+            for i, user in enumerate(data["users"][:5]):
+                result_item = f'''
+                    <div class="result-item">
+                        <h3 class="title">{user["firstName"]} {user["lastName"]}</h3>
+                        <p class="label">전화번호: {user["phoneNumber"]}</p>
+                        <p class="label">이메일: {user.get("emailAddress", "")}</p>
+                        <a href="{user["homepage"]}" class="top-right-btn" target="_blank">사이트 이동</a>
+                        <a class="bottom-right-btn" target="_blank">상세 정보</a>
+                        <button class="bottom-right-btn">상세 정보</button>
+                    </div>
+                '''
+                st.markdown(result_item, unsafe_allow_html=True)
+
+            st.markdown('</div>', unsafe_allow_html=True)
+
+# 연결 종료
+conn.close()
+
