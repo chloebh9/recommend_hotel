de45b47466eb25cef618954bf43b481260f551c3
co2ma <67058333+co2ma@users.noreply.github.com>
2024-07-19 18:37:38 +0900

    Update get_information.py

--- get_information.py
+++ get_information.py
@@ -5,11 +5,10 @@ import requests
 import json
 
 def get_information(submit_json):
-  csv_file = "/root/LLM_Bootcamp/Project3/cleaned_top_200_rows.csv"
-  
-  # JSON 객체를 문자열로 변환
+  csv_file = "exercise_3/data/cleaned_top_200_rows.csv"
+
   submit_json = json.dumps(submit_json)
-    
+
   def get_img(keyword):
     base_url = "http://apis.data.go.kr/B551011/KorService1/searchKeyword1"
     api_key = os.getenv("DATA_API_KEY")   # .env 파일에 각자 API 추가
@@ -45,11 +44,13 @@ def get_information(submit_json):
   def search_information(csv_file, ans_json):
     
     df = pd.read_csv(csv_file)
+
     
     # JSON 문자열을 Python 객체로 변환
-    ans_json = json.loads(ans_json)
-    hotel_ids = ans_json.get('hotel_id', [])
-    introduction = ans_json.get('introduction', [])
+    dict_json = json.loads(ans_json)
+  #여기------------기------------기------------기------------
+    hotel_ids = dict_json.get('hotel_id', []) 
+    introduction = dict_json.get('introduction', [])
     
     # csv파일 hotel_id가 문자열 형식인지 확인 및 변환
     df['명칭'] = df['명칭'].astype(str)
@@ -88,7 +89,7 @@ def get_information(submit_json):
   return result
 
 if __name__ == "__main__":
-  load_dotenv()  # .env 파일에서 환경 변수 로드
+  load_dotenv()  # 환경 변수 로드
   
   csv_file = "/root/LLM_Bootcamp/Project3/cleaned_top_200_rows.csv"
   ans_json = '{"introduction" : "asdf", "hotel_id" : ["백년한옥", "엘리스호텔"]}' # 예시(json으로 받아온거)
